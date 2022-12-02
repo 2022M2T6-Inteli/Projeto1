4a69df042f0676f244981e6a1104b151f27ea1cc
MouraDWG <guilherme.moura@sou.inteli.edu.br>
2022-12-02 02:14:39 -0300

    Commit Cadastro Atualizado

--- src/backend/routes/empreiteiraProfile.js
+++ src/backend/routes/empreiteiraProfile.js
@@ -41,9 +41,9 @@ router
         db.close();
     })
 
-    router
+router
     .route('/infoResp')
-    .get((req, res) => {
+    .post((req, res) => {
         var db = new sqlite3.Database(DBPATH); // Instantiate database
         var sql = `SELECT * FROM Responsavel_Empreiteira WHERE ID_Empreiteira = ${req.query.id};`;
 	    console.log(sql);
@@ -55,6 +55,58 @@ router
         });
         db.close();
     })
-
+router
+    .route("/atualizarGET")
+    .get((req,res)=>{
+        var db = new sqlite3.Database(DBPATH);
+        var sql = `SELECT * FROM Empreiteira WHERE ID_Empreiteira = ${req.query.id};`;
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+    })
+router
+    .route("/mandarAtual")
+    .post((req,res)=>{
+        var db = new sqlite3.Database(DBPATH);
+        var sql = `UPDATE Empreiteiras SET `
+    })
 
 module.exports = router;
+
+
+// //LER POSTAGEM PARA UPTADE
+// app.get('/postagem/atualizar', (req, res) => {
+//     res.statusCode = 200; //Código de status HTTP, que indica pro cliente qual a situação da sua requisição
+//     res.setHeader('Access-Control-Allow-Origin', '*'); 
+//     var db = new sqlite3.Database(DBPATH); // Abre o banco
+// 	var sql = `SELECT Titulo, Descricao, Data_Inicio, ID_Oportunidade FROM Oportunidade WHERE ID_Oportunidade= '${req.query.ID_Oportunidade}'`;
+// 	console.log(sql)
+//     db.all(sql, [],  (err, rows ) => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.json(rows);
+//     });
+//     db.close(); // Fecha o banco
+// });
+
+// //UPDATE POSTAGEM
+// app.post('/postagem/atualizar', urlencodedParser, (req, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+// 	var db = new sqlite3.Database(DBPATH); // Abre o banco
+// 	var sql = `UPDATE Oportunidade SET Titulo= '${req.body.Titulo}', Descricao= '${req.body.Descricao}', Data_Inicio= '${req.body.Data_Inicio}' WHERE ID_Oportunidade= '${req.body.ID_Oportunidade}'`; //Lê-se: "update a tabela costumers, colocando (set) no parâmetro adress o termo ... no lugar (where) do termo ...""
+// 	console.log(sql)
+// 	db.run(sql, [],  err => {
+// 		if (err) {
+// 		    throw err;
+// 		}
+// 		res.send();
+// 	});
+// 	db.close(); // Fecha o banco
+// 	res.write('<p>Postagem atualizada com sucesso!</p><a href="/lerPost.html">Voltar</a>')
+// });
--- src/frontend/scripts/signup.js
+++ src/frontend/scripts/signup.js
@@ -3,15 +3,8 @@ function submitProfile(){
     let razaoSoc = $('#razaoSoc').val();
     let nomeFant = $('#nomeFant').val();
     let cnae = $('#cnae').val();
-    let insc = $('#insc').val();
     let dataAbert = $('#dataAbert').val();
     let email = $('#email').val();
-    let rua = $('#rua').val();
-    let n = $('#n').val();
-    let bairro = $('#bairro').val();
-    let estados = $('#estados').val();
-    let cidades = $('#cidades').val();
-    let func = $('#func').val();
     let serv1 = $('#serv1').val();
     let serv2 = $('#serv2').val();
     $.ajax({
@@ -22,15 +15,8 @@ function submitProfile(){
             razaoSoc: razaoSoc, 
             nomeFant: nomeFant, 
             cnae: cnae, 
-            insc: insc, 
             dataAbert: dataAbert, 
             email: email, 
-            rua: rua, 
-            n: n, 
-            bairro: bairro, 
-            estados: estados, 
-            cidades: cidades, 
-            func: func, 
             serv1: serv1, 
             serv2: serv2
         }
@@ -44,36 +30,37 @@ function submitProfile(){
         console.log('always');
     });
 };
-$(document).ready(function () {
-// Run states and cities function (Shoutout to ografael)
-$.getJSON('https://gist.githubusercontent.com/ografael/2037135/raw/5d31e7baaddd0d599b64c3ec04827fc244333447/estados_cidades.json', function (data) {
-    var items = [];
-    var options = '<option value="">Escolha um estado</option>';	
 
-    $.each(data, function (key, val) {
-        options += '<option value="' + val.nome + '">' + val.nome + '</option>';
-    });					
-    $("#estados").html(options);				
-    
-    $("#estados").change(function () {				
-    
-        var options_cidades = '';
-        var str = "";					
-        
-        $("#estados option:selected").each(function () {
-            str += $(this).text();
-        });
-        
-        $.each(data, function (key, val) {
-            if(val.nome == str) {							
-                $.each(val.cidades, function (key_city, val_city) {
-                    options_cidades += '<option value="' + val_city + '">' + val_city + '</option>';
-                });							
-            }
-        });
+// $(document).ready(function () {
+// // Run states and cities function (Shoutout to ografael)
+// $.getJSON('https://gist.githubusercontent.com/ografael/2037135/raw/5d31e7baaddd0d599b64c3ec04827fc244333447/estados_cidades.json', function (data) {
+//     var items = [];
+//     var options = '<option value="">Escolha um estado</option>';	
 
-        $("#cidades").html(options_cidades);
+//     $.each(data, function (key, val) {
+//         options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+//     });					
+//     $("#estados").html(options);				
+    
+//     $("#estados").change(function () {				
+    
+//         var options_cidades = '';
+//         var str = "";					
         
-    }).change();		
-});
-});
+//         $("#estados option:selected").each(function () {
+//             str += $(this).text();
+//         });
+        
+//         $.each(data, function (key, val) {
+//             if(val.nome == str) {							
+//                 $.each(val.cidades, function (key_city, val_city) {
+//                     options_cidades += '<option value="' + val_city + '">' + val_city + '</option>';
+//                 });							
+//             }
+//         });
+
+//         $("#cidades").html(options_cidades);
+        
+//     }).change();		
+// });
+// });
