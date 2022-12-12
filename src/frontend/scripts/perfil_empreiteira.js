10c6bb26f1bb981502d89a3b192b83d252119317
MouraDWG <guilherme.moura@sou.inteli.edu.br>
2022-12-12 02:24:39 -0300

    Commit 12/12 Guilherme Moura
    Commit com o Editar Perfil Empreiteira, Editar Perfil Responsável Empreiteira e com os Joins da tabela do responsavel com a empreiteira feitos

--- src/frontend/scripts/perfil_empreiteira.js
+++ src/frontend/scripts/perfil_empreiteira.js
@@ -36,6 +36,11 @@ function list() {
                 $('#icon_user').attr('src', element.Foto); // Set Profile picture
                 $('#funcionarios_empreiteira_value').text(`${element.Numero_Funcionarios} Funcionários`) // Set number of employees
                 $('#cnpj').text(element.CNPJ);
+                $('#responsavel_cpf').text(element.CPF);
+                $('#responsavel_email').text(element.Email_Responsavel);
+                $('#responsavel_nome').text(element.Nome);
+                $('#responsavel_celular').text(element.Celular);
+                
                 
 
                 // $('#cnpj_empreiteira').html(element.CNPJ);
@@ -45,73 +50,18 @@ function list() {
         }
     });
     console.log(cnpj);
-    $.ajax({
-        url: `./infoResp/?id=${id}`,
-        type: 'GET',
-        success: data => {
-            data.forEach(element => {
-                $('#cpf_resp').text(element.CPF);  // Set CPF
-                $('#email_resp').text(element.Email);  // Set Responsible Email
-                $('#name_resp').text(element.Nome);  // Set Responsible Name
-                $('#cel_resp').text(element.Celular);  // Set Responsible Email
-                
-
-                // Check number of services to set them
-                if(element.Servico_2 != null){ 
-                    $('#service_empreiteira').text(`${element.Servico_1}, ${element.Servico_2}`);
-                }
-                else{
-                    $('#service_empreiteira').text(element.Servico_1);
-                }
-
-                $('#email').text(element.Email); // Set Email address
-                $('#open_date_value').text(element.Data_Abertura) // Set Opening date
-                $('#icon_user').attr('src', element.Foto); // Set Profile picture
-                $('#funcionarios_empreiteira_value').text(`${element.Numero_Funcionarios} Funcionários`) // Set number of employees
-            });
-        }
-    });
+   
     
 }
 
 $(document).ready(function () {
     list();
-    // ShowdataInput();
     
 });
-
-// var get_url_empreiteira= new URL(window.location.href);
-// var get_url_empreiteira= `/empreiteira/atualizar?id=${url.searchParams.get("id")}`
-// console.log(url.searchParams.get("id"))
-
-// var takejson;
-
-// fetch(get_url_empreiteira).then((response) => {
-//     takejson= response.clone();
-//     return response.json();
-// })
-//     .then(function(response) {
-//         console.log(response)
-//         let empreiteiras= response
-//         let saida= '';
-//         empreiteiras.map(function(empreiteira) {
-//             saida += empreiteira.CNPJ
-//     }, function(rejectionReason){
-//         console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
-//         textjson.text();
-//     }, function(bodyText){
-//         console.log('Received the following instead of valid JSON:', bodyText); // 6
-//     })
-//         document.getElementById('cnpj_empreiteira').innerHTML= saida;
-// })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
 
 
 function ShowdataInput(){
     // let takejson;
-
     $.ajax({
         url: '/perfil/empreiteira/atualizar?id='+url.searchParams.get("id"),
         type: "get",
@@ -140,33 +90,27 @@ function ShowdataInput(){
     })
 }
 
-// function SendeDataEmpreiteira(){
-//     $.post
-// }
-
-// $.post({
-//     url: '/perfil/empreiteira/atualizar?id='+url.searchParams.get("id"),
-//     type: "post",
-//     success: data => {
-//         // console.log(data)
-        
-//         // data= JSON.parse(data);
-//         // console.log(data)
-//         data.forEach(element => {
-//             // $('#cnpj_empreiteira').text(element.CNPJ);
-//             console.log(element.CNPJ)
-//               document.getElementById('cnpj_empreiteira').value= element.CNPJ
-//               document.getElementById('nome_fantasia_empreiteira').value= element.Nome_Fantasia
-//               document.getElementById('razao_social_empreiteira').value= element.Razao_Social
-//               document.getElementById('email_empreiteira').value= element.Email
-//               document.getElementById('cnae_empreiteira').value= element.CNAE
-//               document.getElementById('data_abertura_empreiteira').value= element.Data_Abertura
-//               document.getElementById('numero_funcionarios').value= element.Numero_Funcionarios
-//               document.getElementById('servico_primario_empreiteira').value= element.Servico_1
-//               document.getElementById('servico_secundario_empreiteira').value= element.Servico_2
-              
-//         })
-//     }
-// })
-// }
+function ShowdataInput_Responsavel(){
+    // let takejson;
+    $.ajax({
+        url: '/perfil/responsavel/atualizar?id='+url.searchParams.get("id"),
+        type: "get",
+        success: data => {
+            // console.log(data)
+            
+            // data= JSON.parse(data);
+            // console.log(data)
+            data.forEach(element => {
+                // $('#cnpj_empreiteira').text(element.CNPJ);
+                  document.getElementById('cpf_editar_responsavel').value= element.CPF
+                  document.getElementById('nome_editar_responsavel').value= element.Nome
+                  document.getElementById('email_editar_responsavel').value= element.Email_Responsavel
+                  document.getElementById('celular_editar_responsavel').value= element.Celular
+                  document.getElementById('id_responsavel').value= element.ID_Responsavel
+                console.log("O valor do ID_Responsavel é de:" + element.ID_Responsavel)
+                  
+            })
+        }
+    })
+}
 
