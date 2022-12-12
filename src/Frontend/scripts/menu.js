e5755381630ce139d561abb8b7c7f1bcf24b1199
gabriel-gmc <gabriel.coutinho@sou.inteli.edu.br>
2022-12-12 12:44:36 -0300

    dunno

--- src/Frontend/scripts/menu.js
+++ src/Frontend/scripts/menu.js
@@ -1,5 +1,7 @@
 let urlHost = window.location.origin;
+id = url.searchParams.get("id");
 
 function redirect(page){
      window.location.href = `${urlHost}/${page}`;
+     console.log(`${urlHost}/${page}/?id=${id}`);
 };
